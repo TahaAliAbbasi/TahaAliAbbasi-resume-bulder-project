3c7557813eb1ce652b7e081b29a754fef35a60e2
TahaAliAbbasi <163841832+TahaAliAbbasi@users.noreply.github.com>
2024-09-17 21:00:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggled() {
+    var div = document.getElementById("toggled");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
---
+++ script.ts
@@ -0,0 +1,14 @@
+// function toggled(){
+//     let div = document.getElementById("toggled") as HTMLButtonElement;
+//     if(div.style.display === "none"){
+//         div.style.display = "block";
+//     }
+//     else{
+//         div.style.display = "none";
+//     }
+// }
+
+
+
+// It was showing this error after compilation so i commented TS code
+// Duplicate function implementation.
